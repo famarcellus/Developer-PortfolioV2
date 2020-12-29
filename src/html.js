01e7177bb09b5a57b5f2896976b388b14ed1b619
Fabio <famarcellus12@gmail.com>
2020-12-28 21:37:41 -0500

    Added SEO

--- src/html.js
+++ src/html.js
@@ -1,24 +1,51 @@
 import React from "react"
 import PropTypes from "prop-types"
+import { Helmet } from "react-helmet"
 
 export default function HTML(props) {
   return (
-    <html {...props.htmlAttributes}>
+    <html lang="en" {...props.htmlAttributes}>
       <head>
-        <title>Fabio's Portfolio</title>
+        <title>Fabio's Front End Developer Portfolio</title>
         <meta
-          name="description="content="Fabio's Front End Developer Portfolio provides information about his Front End Development skills, projects he has worked on, and background info on himself."
+          name="description" content="Fabio's Front End Developer Portfolio provides background info about himself, skills used on the Front End, and previous projects that have been completed."
         />
+        <meta name="author" content="Fabio Marcellus" />
         <meta charSet="utf-8" />
         <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+        <meta httpEquiv="content-language" content="en-us" />
         <meta
           name="viewport"
           content="width=device-width, initial-scale=1, shrink-to-fit=no user-scalable=no"
         />
-        <link rel="apple-touch-icon" sizes="180x180" href="PUBLIC_URL/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="PUBLIC_URL/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="PUBLIC_URL/favicon-16x16.png" />
-        <link rel="manifest" href="PUBLIC_URL/site.webmanifest" />
+        {/* Facebook OpenGraph tags */}
+        <meta property="og:title" content="Fabio's Portfolio" />
+        <meta property="og:type" content="website" />
+        <meta property="og:description" content="Portfolio of Front End Developer Fabio Marcellus." />
+        <meta property="og:url" content={process.env.PUBLIC_URL} />
+        <meta property="og:locale" content="en_US" />
+        <meta property="og:site_name" content="Fabio Marcellus" />
+        {/* Twitter cards tags */}
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:title" content="Fabio Marcellus Front End Developer" />
+        <meta name="twitter:description" content="Portfolio of Front End Developer Marcellus." />
+        <link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png" />
+        <link rel="manifest" href="site.webmanifest" />
+        <Helmet>
+          <script type="application/ld+json">
+          {`
+            {
+              "@context": "https://schema.org",
+              "@type": "Person",
+              "jobTitle": "Front End Developer",
+              "name": "Fabio Marcellus",
+              "url": process.env.PUBLIC_URL
+            }
+          `}
+          </script>
+        </Helmet>
         {props.headComponents}
       </head>
       <body {...props.bodyAttributes}>
