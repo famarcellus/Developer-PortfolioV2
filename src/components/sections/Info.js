a141c196567da1c9d6fba411fbc4ddcb1ab656a4
Fabio <famarcellus12@gmail.com>
2021-01-04 13:50:16 -0500

    Added Info Section w/ CTA button

---
+++ src/components/sections/Info.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+function Info() {
+    return (
+        <section className="flex-container" id="info" >
+            <h1 className="name">Hi, my name is <span className="color">Fabio Marcellus</span></h1>
+            <h2 className="job">I'm a <span className="title">Front End Developer</span></h2>
+            <button className="myButton"><a href="#about-section" className="">FIND OUT MORE!</a></button>
+        </section>
+    )
+}
+
+export default Info;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,12 @@
 import React from "react"
 import Navbar from "../components/sections/Navbar"
+import Info from "../components/sections/Info"
 
 export default function Home() {
   return (
     <div>
     <Navbar />
+    <Info />
     </div>
   )
 }
