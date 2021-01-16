7b88e071a131874cb9eee2f8204601654ddacf84
Fabio <famarcellus12@gmail.com>
2021-01-15 22:46:05 -0500

    Fixed bug in individualproject component

--- src/components/sections/IndividualProject.js
+++ src/components/sections/IndividualProject.js
@@ -24,19 +24,11 @@ function IndividualProject(props) {
 
     return (
         <article className="single-project">
-<<<<<<< HEAD
-            <h2 className="">{props.title}</h2>
-            <img className="project-image" src={props.image} alt={props.title} width="100%" height="auto" />
-            <a href={props.sourceLink} tabIndex="-1"><button className="myButton">Source Link</button></a>
-            <a href={props.liveLink} tabIndex="-1"><button className="myButton">Live Link</button></a>
-            <h3 className="line-height">{props.description}</h3>
-=======
             <h2 className="project-title">{props.title}</h2>
             <img className="screenshot" src={props.image} alt={props.title} width="100%" height="auto" />
             <a href={props.sourceLink} tabIndex="-1"><button className="source-button myButton">Source Link</button></a>
             <a href={props.liveLink} tabIndex="-1"><button className="live-button myButton">Live Link</button></a>
             <h3 className="description light-text line-height">{props.description}</h3>
->>>>>>> wip-refactoring
             <h3 className="tech-title">Technologies Used</h3>
             {props.technologies.map((item) => {
                 return (
