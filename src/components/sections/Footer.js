853019f73e310ae3c1140866327d8fc3d750a50c
Fabio <famarcellus12@gmail.com>
2021-01-15 21:54:22 -0500

    refactor: footer component

--- src/components/sections/Footer.js
+++ src/components/sections/Footer.js
@@ -4,12 +4,12 @@ import LinkedIn from "../../images/social/linkedin.svg"
 
 function Footer() {
     return (
-        <footer id="footer-section" className="no-focus"tabIndex="0" role="contentinfo">
-            <h3>I'm looking for Front End Developer roles that present challenges and offer opportunities for professional growth.</h3>
-            <a href="https://github.com/famarcellus"><img src={Github} className="social-icon" alt="Github Logo"/></a>
-            <a href="https://www.linkedin.com/in/fabio-marcellus-a7b324196/"><img src={LinkedIn} className="social-icon" alt="LinkedIn Logo"/></a>
-            <h3 className="copyright">Designed and created solely by myself</h3>
-            <h3 className="copyright">Fabio Marcellus 2021 &#169;</h3>
+        <footer className="footer-section no-focus" tabIndex="0" role="contentinfo">
+            <h3 className="title color-white">I'm looking for Front End Developer roles that present challenges and offer opportunities for professional growth.</h3>
+            <a className="social-link" href="https://github.com/famarcellus"><img src={Github} className="social-icon" alt="Github Logo"/></a>
+            <a className="social-link" href="https://www.linkedin.com/in/fabio-marcellus-a7b324196/"><img src={LinkedIn} className="social-icon" alt="LinkedIn Logo"/></a>
+            <h3 className="copyright color-white">Designed and created solely by myself</h3>
+            <h3 className="copyright color-white">Fabio Marcellus 2021 &#169;</h3>
         </footer>
     )
 }
