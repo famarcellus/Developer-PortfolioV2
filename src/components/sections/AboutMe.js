ba9cdb4d70011ae485d978fab9efc014d0626712
Fabio <famarcellus12@gmail.com>
2021-01-15 19:18:56 -0500

    refactor: about me component

--- src/components/sections/AboutMe.js
+++ src/components/sections/AboutMe.js
@@ -2,12 +2,15 @@ import React from "react"
 
 function AboutMe() {
     return (
-        <section className="display-flex line-height no-focus" id="about-section" tabIndex="0" role="article">
-            <h1 className="rel no-focus">About Me</h1>
-            <h2 className="rel no-focus">I am a Front End Developer finishing up my last year to receive my <span>Bachelor's Degree in Computer Science.</span> I enjoy working 
-            on the Front End using <span>React</span> and I'm also familiar with the Back End of applications. 
+        <section className="about-section justify-center flex-column line-height no-focus" tabIndex="0" role="article">
+            <h1 className="title header-text align-s-center rel no-focus">About Me</h1>
+            <h2 className="content rel no-focus">I am a Front End Developer finishing up my last year to receive my <span className="bold">Bachelor's Degree in Computer Science.</span> I enjoy working 
+            on the Front End using <span className="bold">React</span> and I'm also familiar with the Back End of applications. 
             </h2>
-            <h2 className="rel no-focus">I focus on making <span>accessible</span> web applications that are designed <span>mobile-first,</span> are <span>SEO</span>-friendly, but most importantly, ensure it is <span>responsive.</span> My greatest strengths are my attention to detail and using creative means to solve problems. 
+            <h2 className="content rel no-focus">I focus on making <span className="bold">accessible</span> web applications that are designed  
+            <span className="bold"> mobile-first,</span> are <span className="bold">SEO</span>-friendly, but most importantly, 
+            ensure it is <span className="bold">responsive.</span> My greatest strengths are my attention to detail and using 
+            creative means to solve problems. 
             </h2>
         </section>
     )
--- src/components/sections/Info.js
+++ src/components/sections/Info.js
@@ -2,7 +2,7 @@ import React from "react"
 
 function Info() {
     return (
-        <section className="info-section flex-column align-center">
+        <section className="info-section flex-column align-i-center">
             <h1 className="intro">Hi, my name is <span className="name color-dark-cyan">Fabio Marcellus</span></h1>
             <h2 className="statement">I'm a <span className="job-title color-dark-orange">Front End Developer</span></h2>
             <a href="#about-section" className="cta" tabIndex="-1"><button className="ctaButton animated">FIND OUT MORE!</button></a>
