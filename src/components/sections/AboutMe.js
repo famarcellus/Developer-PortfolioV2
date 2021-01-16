4e1989c236744c152d5174818d1accf6b2003e28
Fabio <famarcellus12@gmail.com>
2021-01-15 20:08:16 -0500

    refactor: projects and individualproject components

--- src/components/sections/AboutMe.js
+++ src/components/sections/AboutMe.js
@@ -3,11 +3,11 @@ import React from "react"
 function AboutMe() {
     return (
         <section className="about-section justify-center flex-column line-height no-focus" tabIndex="0" role="article">
-            <h1 className="title header-text align-s-center rel no-focus">About Me</h1>
-            <h2 className="content rel no-focus">I am a Front End Developer finishing up my last year to receive my <span className="bold">Bachelor's Degree in Computer Science.</span> I enjoy working 
+            <h1 className="title header-text align-s-center relative no-focus">About Me</h1>
+            <h2 className="content relative no-focus">I am a Front End Developer finishing up my last year to receive my <span className="bold">Bachelor's Degree in Computer Science.</span> I enjoy working 
             on the Front End using <span className="bold">React</span> and I'm also familiar with the Back End of applications. 
             </h2>
-            <h2 className="content rel no-focus">I focus on making <span className="bold">accessible</span> web applications that are designed  
+            <h2 className="content relative no-focus">I focus on making <span className="bold">accessible</span> web applications that are designed  
             <span className="bold"> mobile-first,</span> are <span className="bold">SEO</span>-friendly, but most importantly, 
             ensure it is <span className="bold">responsive.</span> My greatest strengths are my attention to detail and using 
             creative means to solve problems. 
--- src/components/sections/IndividualProject.js
+++ src/components/sections/IndividualProject.js
@@ -24,15 +24,15 @@ function IndividualProject(props) {
 
     return (
         <article className="single-project">
-            <h2 className="">{props.title}</h2>
-            <img className="project-image" src={props.image} alt={props.title} width="100%" height="auto" />
+            <h2 className="project-title">{props.title}</h2>
+            <img className="screenshot" src={props.image} alt={props.title} width="100%" height="auto" />
             <a href={props.sourceLink} tabIndex="-1"><button className="myButton">Source Link</button></a>
-            <a href={props.liveLink} tabIndex="-1" ><button className="myButton">Live Link</button></a>
-            <h3 className="line-height">{props.description}</h3>
+            <a href={props.liveLink} tabIndex="-1"><button className="myButton">Live Link</button></a>
+            <h3 className="description light-text line-height">{props.description}</h3>
             <h3 className="tech-title">Technologies Used</h3>
             {props.technologies.map((item) => {
                 return (
-                    <h3 className="technologies rel">{item} <img src={FindTechnology(item)} alt={item}></img></h3>
+                    <h3 className="tech-name light-text">{item} <img className="logo" src={FindTechnology(item)} alt={item}></img></h3>
                 )
             })}
         </article>
